7bb7ae49b26f0c66da4d5652f1dcb013ce59f9ba
zaheerkhan12 <za7058143@gmail.com>
2024-05-20 00:41:49 -0700

    java class

---
+++ index.js
@@ -0,0 +1,301 @@
+
+
+
+
+
+//  const num = "233"
+// const  num2= 4575
+// console.log(num + num2)
+
+
+
+// var num = "4444"
+// var num2 = 55555 ;
+// console.log (num * num2)
+
+
+// var num1 = 777 ;
+// var num = 888 ;
+// console.log(num1 / num)
+
+
+// const num = 999 ;
+// const num3 = 555 ;
+// console.log(num - num3)
+
+
+// const firstname = "Zaheer Ali"
+// const lastname ="Soomro "
+// console.log(firstname + " " + lastname )
+
+
+// let firstname = "zaheer"
+// let lastname = "shah"
+// console.log(firstname   + " " +lastname)
+
+// num = 776;
+// num =33;
+// console.log (num)
+
+
+// num = 776;
+// num = 33;
+// console.log  (num)
+
+
+// const email = "soomrozaheerkhan449gmail.com"
+// const name = "zaheer ali soomro"
+// const account = 4510216375059 ;
+
+// console.table ([email , name , account  ])
+
+
+
+// let email = "soomrozaheerkhan449@gmail.com"
+// let name = "zaheer ali "
+// let account = 4567675787856;
+// let etc
+// console.table([email , name , account , etc])
+
+
+// math operatos = + - * /   divided % modules
+
+// let num = 66;
+// let num2 = 1;
+// console.log (num + num2);
+
+// let num = "66";
+// let num2 = 1;
+// console.log(num + num2);
+
+
+// let data = 100;
+// let data2 = 5;
+// console.log(data * data2);
+
+// let data = 100;
+// let data2 = 5;
+// console.log(data /  data2)
+
+// let data = 10;
+// let data2 = 3;
+// let add = data % data2;
+// console.log(add);
+
+// let data = 11;
+// let data2 = 3;
+// let add =data % data2;
+// console.log(add);
+
+// let name = prompt("what is your name...........");
+// document.write(name)
+
+// const name  = prompt ("what uis your name ............")
+// console.log(name )
+
+
+
+// let name = prompt ("what is your number...........");
+// console.log(name);
+
+// let rendomnumber1 = math.random();
+// console.log(rendomnumber1)
+
+
+
+// let rendomNumber = Math.random();
+// rendomNumber = rendomNumber * 10;
+// let rounded = Math.round(rendomNumber);
+// console.log (rounded);
+// BODMAS = BARKET OPEN DIVIDED MULPLY ADD SUBTRA
+    //  let num = 4 + 6 * ( 5 + 5);
+    //  console.log(num);
+
+
+
+
+// let x = 33;
+// console.log(x);
+// // console.log(typeof string (x));
+
+// let name = null;
+// console.log(name);
+
+
+// const name = null ;
+// console.log(name )
+
+
+
+// const x = 33 ;
+// const y = true ;
+// console.log(x + y) ;
+
+
+// boolean 
+// let x = 33;
+//  let y = false;
+//  console.log(x);
+
+// console.log (typeof y);
+// console.log(x + y);
+
+// const x = 33;
+// const = fasle ;
+// console.log(x + 12 )
+
+
+
+
+
+// let x = 33;
+// let = true;
+// console.log(x + 12 );
+
+
+
+// let num = null
+// console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+    //  let num =prompt("what is");
+    //  console.log(+num +5);
+// comperison operator
+// == ===
+// let num1 = 6; //data type number
+// let num2 = '6'; //string
+// console.log(num1 == num2);
+
+
+// let num1 = 6; //data type number
+//  let num2 = '6'; //string
+//  console.log(num1 === num2);
+
+// let num1 = 6; //data type number
+//  let num2 = '6'; //string
+//  console.log(num1 !== num2);
+// /console.log("5" !== 5)
+//  < greater then
+// > less then
+
+// console.log(5 > 7);
+// console.log(5 < 7);
+
+// let num = 55;
+// let num2 = 66;
+// console.log(num < num2);
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num > num2);
+
+
+// <= <== 
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num <= num2);
+
+
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num >= num2);
+
+
+    // let num = 155;
+    // let num2 = 154;
+    // console.log(num <= num2);
+
+
+
+
+
+
+// let num = 155;
+// let num2 = 9999;
+// console.log(num <= num2);
+
+
+// let age = +prompt("inter your age");
+
+// if(age >= 18 ){
+//     console.log(" you can apply cnic");
+// }else{
+//     console.log("you are not eligble")
+// }
+
+
+// let age = +prompt("inter your age");
+
+// if(age >= 18 ){
+//     console.log(" you can apply cnic");
+// }else{
+//     console.log("you are not eligble")
+// }
+
+
+// let userNum = +prompt("inter your number");
+// let computerNumber = Math.round(Math.random() * 5)
+// if(userNum === computerNumber){
+//     console.log("you win");
+// }else {
+//     console.log("you loss !"+ computerNumber);  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
